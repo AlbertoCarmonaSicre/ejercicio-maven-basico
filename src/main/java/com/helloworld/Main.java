b94b360a38af9533e68abe1186ffd5fda040cd52
alberto.carmona <alberto.carmona@babelgroup.com>
2025-03-10 10:37:45 +0100

    Añadir método jsonToPersona

--- src/main/java/com/helloworld/Main.java
+++ src/main/java/com/helloworld/Main.java
@@ -1,8 +1,14 @@
 package com.helloworld;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.gson.Gson;
+
 public class Main {
+
+    public static Persona jsonToPersona(String json){
+        Gson gson = new Gson();
+        return gson.fromJson(json, Persona.class);
+    }
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- src/main/java/com/helloworld/Persona.java
+++ src/main/java/com/helloworld/Persona.java
@@ -3,4 +3,15 @@ package com.helloworld;
 public class Persona {
     private String nombre;
     private Integer edad;
+
+    public Persona() {}
+
+    public Persona(String nombre, Integer edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String toString(){
+        return "Persona{nombre='" + nombre + "', edad=" + edad + "}";
+    }
 }
